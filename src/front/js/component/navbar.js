e0aa6a7da110c36487931567854430600d845c2f
ParraJuanPablo <117546451+ParraJuanPablo@users.noreply.github.com>
2023-07-11 17:52:29 +0000

    Registro, Inicio de Sesión y Principio de Home-page

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../../img/parra-y-trebejos-logo.png";
+import "../../styles/component/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+	<nav className="navbar navbar-expand-lg bg-body-tertiary">
+		<div id="navbar" className="container-fluid d-flex justify-content-between">
+			<div>
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src={logo} alt="Parras & Trebejos Consultores C.A." height="60" />
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+			</div>
+			<div>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarToggler">
+					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+						<li className="nav-item">
+							<Link to="/contact">
+								<i class="fa-solid fa-comment-dots"></i>
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/about">
+								<i className="fa-solid fa-circle-info"></i>
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to="/sign">
+								<i className="fa-solid fa-right-to-bracket"></i>
+							</Link>
+						</li>
+					</ul>
 				</div>
 			</div>
-		</nav>
+		</div>
+	</nav>
 	);
 };
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,6 +4,8 @@ import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
 import { Home } from "./pages/home";
+import { Sign } from "./pages/sign";
+
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
@@ -26,6 +28,7 @@ const Layout = () => {
                     <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
+                        <Route element={<Sign />} path="/sign" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
